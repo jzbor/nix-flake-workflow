3e4b2324cdecd01c504483117699323a5e6ee8e2
jzbor <jzbor@posteo.net>
2024-09-03 01:15:19 +0200

    Fixing flake

--- flake.nix
+++ flake.nix
@@ -11,67 +11,69 @@
   let
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    apps.add = let
-      workflowFile = ".github/workflows/flake.yml";
-      script = pkgs.writeShellApplication {
-        name = "add";
-        text = ''
-          if [ -f ${workflowFile} ]; then
-            echo "Workflow file \"${workflowFile}\" already exists" >&2
-            exit 1
-          fi
+    apps = {
+      add = let
+        workflowFile = ".github/workflows/flake.yml";
+        script = pkgs.writeShellApplication {
+          name = "add";
+          text = ''
+            if [ -f ${workflowFile} ]; then
+              echo "Workflow file \"${workflowFile}\" already exists" >&2
+              exit 1
+            fi
 
-          mkdir -pv "$(dirname ${workflowFile})"
-          cp -v ${self}/template.yml ${workflowFile}
-          chmod 600 ${workflowFile}
-        '';
+            mkdir -pv "$(dirname ${workflowFile})"
+            cp -v ${self}/template.yml ${workflowFile}
+            chmod 600 ${workflowFile}
+          '';
+        };
+      in {
+        type = "app";
+        program = "${script}/bin/add";
       };
-    in {
-      type = "app";
-      program = "${script}/bin/add";
-    };
 
-    apps.add-fast = let
-      workflowFile = ".github/workflows/flake-fast.yml";
-      script = pkgs.writeShellApplication {
-        name = "add-fast";
-        text = ''
-          if [ -f ${workflowFile} ]; then
-            echo "Workflow file \"${workflowFile}\" already exists" >&2
-            exit 1
-          fi
+      add-fast = let
+        workflowFile = ".github/workflows/flake-fast.yml";
+        script = pkgs.writeShellApplication {
+          name = "add-fast";
+          text = ''
+            if [ -f ${workflowFile} ]; then
+              echo "Workflow file \"${workflowFile}\" already exists" >&2
+              exit 1
+            fi
 
-          mkdir -pv "$(dirname ${workflowFile})"
-          cp -v ${self}/template-fast.yml ${workflowFile}
-          chmod 600 ${workflowFile}
-        '';
+            mkdir -pv "$(dirname ${workflowFile})"
+            cp -v ${self}/template-fast.yml ${workflowFile}
+            chmod 600 ${workflowFile}
+          '';
+        };
+      in {
+        type = "app";
+        program = "${script}/bin/add-fast";
       };
-    in {
-      type = "app";
-      program = "${script}/bin/add-fast";
-    };
 
-    # This mainly suits NixOS-based installations of attic
-    apps.create-attic-token = let
-      script = pkgs.writeShellApplication {
-        name = "create-attic-token";
-        text = ''
-          if [ "$#" -lt 1 ]; then
-            echo "Usage: $0 <ssh-host> <cache-name>"
-            exit 1
-          fi
+      # This mainly suits NixOS-based installations of attic
+      create-attic-token = let
+        script = pkgs.writeShellApplication {
+          name = "create-attic-token";
+          text = ''
+            if [ "$#" -lt 1 ]; then
+              echo "Usage: $0 <ssh-host> <cache-name>"
+              exit 1
+            fi
 
-          # shellcheck disable=SC2029
-          token="$(ssh "$1" "cd / && atticd-atticadm make-token --sub github-ci --validity 2y --pull $2 --push $2 2> /dev/null")"
+            # shellcheck disable=SC2029
+            token="$(ssh "$1" "cd / && atticd-atticadm make-token --sub github-ci --validity 2y --pull $2 --push $2 2> /dev/null")"
 
-          echo "ATTIC_ENDPOINT: <your-endpoint>"
-          echo "ATTIC_CACHE: $2"
-          echo "ATTIC_TOKEN: $token"
-        '';
+            echo "ATTIC_ENDPOINT: <your-endpoint>"
+            echo "ATTIC_CACHE: $2"
+            echo "ATTIC_TOKEN: $token"
+          '';
+        };
+      in {
+        type = "app";
+        program = "${script}/bin/create-attic-token";
       };
-    in {
-      type = "app";
-      program = "${script}/bin/create-attic-token";
     };
   });
 }
