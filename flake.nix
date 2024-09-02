eea2da804a2c2b4ab17f461f0a97321b28d60330
jzbor <jzbor@posteo.net>
2024-09-02 18:28:21 +0200

    Adding caching

--- flake.nix
+++ flake.nix
@@ -1,7 +1,36 @@
 {
   description = "A reuseable github workflow for nix flakes";
 
-  inputs = {};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    cf.url = "github:jzbor/cornflakes";
+    cf.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self }: {};
+  outputs = { self, nixpkgs, cf }: cf.lib.flakeForDefaultSystems (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    apps.create-attic-token = let
+      script = pkgs.writeShellApplication {
+        name = "create-attic-token";
+        text = ''
+          if [ "$#" -lt 1 ]; then
+            echo "Usage: $0 <ssh-host> <cache-name>"
+            exit 1
+          fi
+
+          # shellcheck disable=SC2029
+          token="$(ssh "$1" "cd / && atticd-atticadm make-token --sub github-ci --validity 2y --pull $2 --push $2 2> /dev/null")"
+
+          echo "ATTIC_ENDPOINT: <your-endpoint>"
+          echo "ATTIC_CACHE: $2"
+          echo "ATTIC_TOKEN: $token"
+        '';
+      };
+    in {
+      type = "app";
+      program = "${script}/bin/create-attic-token";
+    };
+  });
 }
